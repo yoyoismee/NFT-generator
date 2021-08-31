d3ae58e2002e9260168a85395a91f2cae3351429
yoyoismee <yoyoismee@gmail.com>
2021-08-31 21:49:50 +0700

    refactor

--- generate.py
+++ generate.py
@@ -1,9 +1,6 @@
 import argparse
-from PIL import Image
-from glob import glob
-from numpy.random import choice
-import os
 from pathlib import Path
+from generator import NFTGenerator
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_dir", help="input dir")
@@ -16,37 +13,6 @@ parser.add_argument("--fps", default=4, help="frame per sec")
 args = parser.parse_args()
 
 Path(args.output_dir).mkdir(parents=True, exist_ok=True)
-
-component = [p for p in os.listdir(args.input_dir) if not p.startswith('.')]
-component = sorted(component)
-component = [glob(args.input_dir + f"/{c}/*") for c in component]
-component = [c for c in component if len(c) > 0]
-img_no = 0
-for _ in range(int(args.amount)):
-    frames = []
-    parts = []
-    for part in component:
-        part_option = choice(part, 1)[0]
-        parts.append(part_option)
-        if os.path.isdir(part_option):
-            parts[-1] = [p for p in sorted(glob(part_option + '/*')) if not p.startswith('.')]
-
-    for i in range(int(args.n_frame)):
-        new_img = None
-        for p in parts:
-            if type(p) is list:
-                tmp = Image.open(p[i])
-            else:
-                tmp = Image.open(p)
-            if new_img is None:
-                new_img = tmp
-            else:
-                new_img.paste(tmp, (0, 0), tmp)
-        frames.append(new_img)
-    if args.animate:
-        frames[0].save(f"{args.output_dir}/No_{img_no}.gif", save_all=True, append_images=frames[1:], loop=0,
-                       duration=int(1000 / int(args.fps)))
-    else:
-        frames[0].save(f"{args.output_dir}/No_{img_no}.png")
-
-    img_no += 1
+nft_gen = NFTGenerator(args.input_dir, animate=args.animate, n_frame=args.n_frame, fps=args.fps)
+for i in range(int(args.amount)):
+    img = nft_gen.generate(save_path=args.output_dir, file_name=str(f'{i + 1}'))
---
+++ generator.py
@@ -0,0 +1,54 @@
+import argparse
+import time
+
+from PIL import Image
+from glob import glob
+from numpy.random import choice
+import os
+
+
+class NFTGenerator:
+    def __init__(self, input_dir, animate=False, n_frame=1, fps=1):
+        self.input_dir = input_dir
+        component = [p for p in os.listdir(self.input_dir) if not p.startswith('.')]
+        component = sorted(component)
+        component = [glob(self.input_dir + f"/{c}/*") for c in component]
+        self.component = [c for c in component if len(c) > 0]
+        self.n_frame = n_frame
+        self.animate = animate
+        self.fps = fps
+
+    def generate(self, save_path=None, file_name=None):
+        frames = []
+        parts = []
+        for part in self.component:
+            part_option = choice(part, 1)[0]
+            parts.append(part_option)
+            if os.path.isdir(part_option):
+                parts[-1] = [p for p in sorted(glob(part_option + '/*')) if not p.startswith('.')]
+
+        for i in range(int(self.n_frame)):
+            new_img = None
+            for p in parts:
+                if type(p) is list:
+                    tmp = Image.open(p[i])
+                else:
+                    tmp = Image.open(p)
+                if new_img is None:
+                    new_img = tmp
+                else:
+                    new_img.paste(tmp, (0, 0), tmp)
+            frames.append(new_img)
+        if self.animate:
+            if save_path is not None:
+                if file_name is None:
+                    file_name = str(time.time())
+                frames[0].save(f"{save_path}/{file_name}.gif", save_all=True, append_images=frames[1:], loop=0,
+                               duration=int(1000 / int(self.fps)))
+            return frames
+        else:
+            if save_path is not None:
+                if file_name is None:
+                    file_name = str(time.time())
+                frames[0].save(f"{save_path}/No_{file_name}.png")
+            return frames[0]
